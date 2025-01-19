12f108aede7a9aca1ef31cd6396416bf7621c33d
blase147 <solarmails2@gmail.com>
2025-01-19 15:48:14 +0100

    update auth.config

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,5 @@
 import { NextAuthConfig } from "next-auth";
- 
+
 // import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authConfig: NextAuthConfig = {
@@ -11,16 +11,22 @@ export const authConfig: NextAuthConfig = {
     async authorized({ auth, request: { nextUrl } }: { auth: any, request: { nextUrl: URL } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnLandingPage = nextUrl.pathname === '/';
 
       if (isOnDashboard) {
+        // Allow only logged-in users to access the dashboard
         return isLoggedIn;
+      } else if (isOnLandingPage) {
+        // Allow both logged-in and non-logged-in users to access the landing page
+        return true;
       } else if (isLoggedIn) {
+        // Redirect logged-in users to the dashboard for other restricted pages
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
 
+      // Allow non-logged-in users access to public pages
       return true;
     },
   },
   providers: [],
 };
-
