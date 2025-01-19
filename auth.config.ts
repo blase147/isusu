75f0454ff7036b59afe1150b886f23161d6f1332
blase147 <solarmails2@gmail.com>
2025-01-19 02:57:17 +0100

    update auth

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,13 @@
 import { NextAuthConfig } from "next-auth";
  
-import CredentialsProvider from "next-auth/providers/credentials";
+// import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async authorized({ auth, request: { nextUrl } }: { auth: any, request: { nextUrl: URL } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
@@ -20,22 +21,6 @@ export const authConfig: NextAuthConfig = {
       return true;
     },
   },
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        // Implement your authentication logic here
-        const user = { id: 1, name: "Test User", email: "test@example.com" }; // Dummy data
-        if (user) {
-          return user;
-        }
-        return null;
-      },
-    }),
-  ],
+  providers: [],
 };
 
