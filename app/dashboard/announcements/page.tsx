1b3b3592ea7c675eb03cf4c24a3229b6b612706f
Chukwuma Paul Mosanya <solarmails2@gmail.com>
2025-04-04 20:59:16 +0100

    fix prerendering

--- app/dashboard/announcements/page.tsx
+++ app/dashboard/announcements/page.tsx
@@ -4,6 +4,8 @@ import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import AnnouncementForm from "@/app/ui/isusu/announcements";
 
+export const dynamic = "force-dynamic";
+
 function PageContent() {
     const searchParams = useSearchParams();
     const isusuId = searchParams.get("isusuId");
