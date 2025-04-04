5e2658e14c2dd0c9806526aca0395b3e36d1ad07
Chukwuma Paul Mosanya <solarmails2@gmail.com>
2025-04-04 21:22:06 +0100

    fix prerendering

--- app/dashboard/create-isusu/page.tsx
+++ app/dashboard/create-isusu/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import dynamic from 'next/dynamic';
-export const dynamicConfig = 'force-dynamic';
 
-const CreateIsusu = dynamic(() => import("../../ui/isusu/create-isusu"), { ssr: false });
+const CreateIsusu = dynamic(() => import('./../../ui/isusu/create-isusu'), { ssr: false });
 
 export default function Page() {
     return <CreateIsusu />;
 }
+
+export const metadata = {
+    dynamic: 'force-dynamic',
+};
